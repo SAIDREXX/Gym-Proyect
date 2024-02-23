db9d466352ff714a315e35ca0e9854ab6aedda46
Rafael Said Hernandez Demeneghi <saidrexxdemher@gmail.com>
2024-02-22 23:42:50 -0600

    Endpoints Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), auth()],
+  output: "server",
+  adapter: vercel()
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly TURSO_URL: string;
+  readonly TURSO_AUTH_TOKEN: string;
+  readonly SUPABASE_URL: string;
+  readonly SUPABASE_ANON_KEY: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
