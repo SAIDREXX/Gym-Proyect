f8552f7b57899934d21e19c8c14fb6d9ef4bc89f
Rafael Said Hernandez Demeneghi <saidrexxdemher@gmail.com>
2024-03-12 08:01:26 -0600

    Bento Sections Added; Logout Function Added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,11 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        redgym: "#F70000",
+        orange: "#FF3E01",
+        greengym: "#00ba32"
+      },
       gridTemplateRows: {
         16: "repeat(22, minmax(0, 1fr))",
       },
